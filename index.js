7d7fd60e1cdd2af3fe74f05df5116d2d0536e6d8
jcawesome <pagtalunanjancarlo@hotmail.com>
2023-03-30 16:22:21 +0000

    Updated version

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const add = require("./lib/add.js")
 const subtract = require("./lib/subtract.js")
 const multiply = require("./lib/multiply.js")
 
-module.exports = add;
-module.exports = subtract;
-module.exports = multiply;
+export{add, subtract, multiply}
 console.log("Welcome to jcawesome math")
