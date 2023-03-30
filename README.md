f65cf2e92fab7b9bd994448a1bded069ac729545
jcawesome <pagtalunanjancarlo@hotmail.com>
2023-03-30 16:04:14 +0000

    updated exports for modules

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# math-package
+# Math Package
+Package that contains basic artithemetic operations
+
+## Installation
+npm install jcawesome-math
+
+## Contribution
+TBD
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,8 @@
+const add = require("./lib/add.js")
+const subtract = require("./lib/subtract.js")
+const multiply = require("./lib/multiply.js")
+
+module.exports = add;
+module.exports = subtract;
+module.exports = multiply;
+console.log("Welcome to jcawesome math")
